a43a21c9fe16a948a44292323b509318915264f1
shubham088 <shubhamsharma0296@gmail.com>
2020-12-24 13:14:58 +0530

    added text utlity for capitalizing alternate character

--- utilities/urls.py
+++ utilities/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.text_utilities, name='text_utilities'),
     path('remove_punc', views.remove_punctuations, name="remove_punctuations"),
+    path('capitalize_alternate', views.capitalize_alternate, name='capitalize_alternate'),
 ]
--- utilities/views.py
+++ utilities/views.py
@@ -22,3 +22,24 @@ def remove_punctuations(request):
             analyzed_text = analyzed_text + char
     context_dict  = {"analyzed_text":analyzed_text, "text_to_analyze":text_to_analyze}
     return render(request, 'utilities/remove_punctuation.html', context_dict)
+
+
+def capitalize_alternate(request):
+    if request.method == 'GET':
+        text_to_analyze = request.GET.get('text', 'default')
+        print("this is the text to analyze : ", text_to_analyze)
+    if request.method == 'POST':
+        text_to_analyze = request.POST.get('text', 'default')
+        print("this is the text to analyze : ", text_to_analyze)
+
+    analyzed_text = ""
+    for x in range(0, len(text_to_analyze)):
+        if x%2 == 0:
+            print(text_to_analyze[x])
+            analyzed_text = analyzed_text + text_to_analyze[x].upper()
+        else:
+            print(text_to_analyze[x])
+            analyzed_text = analyzed_text + text_to_analyze[x]
+
+    context_dict  = {"analyzed_text":analyzed_text, "text_to_analyze":text_to_analyze}
+    return render(request, 'utilities/capitalize_alt.html', context_dict)
